2f18a96f810ca400ebfd3994b566bff03143fc57
Denis Chernyak <chernyak.ds@gmail.com>
2024-08-13 20:29:22 +0300

    looks done

---
+++ src/components/Options/Options.jsx
@@ -0,0 +1,10 @@
+export default function Options({ onLeaveFeedback, totalFeedback }) {
+    return (
+       <div>
+    <button onClick={() => onLeaveFeedback('good')}>Good</button>
+    <button onClick={() => onLeaveFeedback('neutral')}>Neutral</button>
+    <button onClick={() => onLeaveFeedback('bad')}>Bad</button>
+    {totalFeedback > 0 && <button onClick={() => onLeaveFeedback('reset')}>Reset</button>}
+  </div>
+    );  
+};
