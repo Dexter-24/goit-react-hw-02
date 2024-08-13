2d68ec3498de56713659ffc47774629222e8daa9
Denis Chernyak <chernyak.ds@gmail.com>
2024-08-13 20:37:24 +0300

    hover added

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,10 +1,12 @@
+import css from './Options.module.css'
+
 export default function Options({ onLeaveFeedback, totalFeedback }) {
     return (
        <div>
     <button onClick={() => onLeaveFeedback('good')}>Good</button>
     <button onClick={() => onLeaveFeedback('neutral')}>Neutral</button>
     <button onClick={() => onLeaveFeedback('bad')}>Bad</button>
-    {totalFeedback > 0 && <button onClick={() => onLeaveFeedback('reset')}>Reset</button>}
+    {totalFeedback > 0 && <button onClick={() => onLeaveFeedback('reset')} className={css.reset}>Reset</button>}
   </div>
     );  
 };
